577f869e78c84efa3b4910f206da8ed4f66f4a51
William Woodruff <william@trailofbits.com>
2022-06-14 10:42:07 -0400

    treewide: test file, summary, etc
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- action.py
+++ action.py
@@ -16,17 +16,26 @@ def _pip_audit(*args):
 
 
 def _fatal_help(msg):
-    print(msg)
+    print(f"❌ {msg}", file=sys.stderr)
     subprocess.run(_pip_audit("--help"))
     sys.exit(1)
 
 
 inputs = [Path(p).resolve() for p in sys.argv[1].split()]
+summary = Path(os.getenv("GITHUB_STEP_SUMMARY")).open("a")
 
 # The arguments we pass into `pip-audit` get built up in this list.
-# We disable the spinner (it's useless in the CI) and use a custom cache
-# directory (until we create a release that contains pip-audit#290).
-pip_audit_args = ["--progress-spinner=off", "--cache-dir=/tmp/pip-audit-cache"]
+pip_audit_args = [
+    # The spinner is useless in the CI.
+    "--progress-spinner=off",
+    # Include full descriptions in the output.
+    "--desc",
+    # Write the output to this logfile, which we'll turn into the step summary (if configured).
+    "--output=/tmp/pip-audit-output.txt",
+]
+
+if os.getenv("GHA_PIP_AUDIT_NO_DEPS", "false") != "false":
+    pip_audit_args.append("--no-deps")
 
 if os.getenv("GHA_PIP_AUDIT_REQUIRE_HASHES", "false") != "false":
     pip_audit_args.append("--require-hashes")
@@ -40,19 +49,26 @@ if (
 # adding any explicit input argument(s).
 # Otherwise, we handle either exactly one project path (a directory)
 # or one or more requirements-style inputs (all files).
-if len(inputs) != 0:
-    for input_ in inputs:
-        if input_.is_dir():
-            if len(inputs) != 1:
-                _fatal_help("pip-audit only supports one project directory at a time")
-            pip_audit_args.append(input_)
-        else:
-            if not input_.is_file():
-                _fatal_help(f"input {input_} does not look like a file")
-            pip_audit_args.extend(["--requirement", input_])
+for input_ in inputs:
+    # Forbid things that look like flags. This isn't a security boundary; just
+    # a way to prevent (less motivated) users from breaking the action on themselves.
+    if str(input_).startswith("-"):
+        _fatal_help(f"input {input_} looks like a flag")
+
+    if input_.is_dir():
+        if len(inputs) != 1:
+            _fatal_help("pip-audit only supports one project directory at a time")
+        pip_audit_args.append(input_)
+    else:
+        if not input_.is_file():
+            _fatal_help(f"input {input_} does not look like a file")
+        pip_audit_args.extend(["--requirement", input_])
 
 status = subprocess.run(_pip_audit(*pip_audit_args))
 if status.returncode == 0:
-    print("🎉 pip-audit exited successfully")
+    print("🎉 pip-audit exited successfully", file=summary)
 else:
-    print("❌ pip-audit found one or more problems")
+    print("❌ pip-audit found one or more problems", file=summary)
+
+    with open("/tmp/pip-audit-output.txt", "r") as io:
+        print(io.read(), file=summary)
